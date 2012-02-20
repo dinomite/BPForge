b76c5e779953d493e11769b15826813caf6b1752
Drew Stephens <drew@dinomite.net>
2012-02-20 16:48:12 -0500

    Add premier line text

--- united.pl
+++ united.pl
@@ -1,5 +1,19 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+use LWP::Simple qw/getstore/;
+# Turn on in-place editing
+BEGIN{ $^I = ""; }
+
+my $boardingDocumentsDir = './Boarding documents_files';
+my $printHTML = $boardingDocumentsDir . '/print.html';
 
 my $seatingGroupURL = 'https://travel.united.com/web/common/images/BP_1.gif';
+
+getstore($seatingGroupURL, $boardingDocumentsDir . '/BP_1.gif');
+
+@ARGV = ($printHTML);
+while (<>) {
+    s/(eco-bp-premier4?-text">)/$1Premier line/g;
+    print;
+}
